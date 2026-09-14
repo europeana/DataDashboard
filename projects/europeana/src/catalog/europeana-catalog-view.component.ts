6e5679cfa2cdf7700a8a993cc65bb9b8d8d5a647
Shwetambara Nazare <nshweta90@gmail.com>
2026-09-14 10:09:54 +0200

    Updates for catalog negotiation

--- projects/europeana/src/catalog/europeana-catalog-view.component.ts
+++ projects/europeana/src/catalog/europeana-catalog-view.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import {
   FilterInputComponent,
   ItemCountSelectorComponent,
+  ModalAndAlertService,
   PaginationComponent,
 } from '@eclipse-edc/dashboard-core';
 import {
+  CatalogDataset,
   CatalogRequestComponent,
   CatalogViewComponent,
+  ContractNegotiationComponent,
+  NegotiationProgressComponent,
 } from '@eclipse-edc/dashboard-core/catalog';
+import { IdResponse } from '@think-it-labs/edc-connector-client';
 import { EuropeanaCatalogCardComponent } from './europeana-catalog-card.component';
 
 @Component({
@@ -25,4 +30,19 @@ import { EuropeanaCatalogCardComponent } from './europeana-catalog-card.componen
   templateUrl: './europeana-catalog-view.component.html',
   styleUrl: '../../../dashboard-core/catalog/src/catalog-view/catalog-view.component.css',
 })
-export class EuropeanaCatalogViewComponent extends CatalogViewComponent {}
+export class EuropeanaCatalogViewComponent extends CatalogViewComponent {
+  private readonly modal = inject(ModalAndAlertService);
+
+  override negotiateContract(catalogDataset: CatalogDataset) {
+    const callbacks = {
+      negotiationRequested: (id: IdResponse) => {
+        this.modal.openModal(NegotiationProgressComponent, { negotiationId: id }, undefined, true);
+      },
+    };
+    this.modal.openModal(
+      ContractNegotiationComponent,
+      { catalogDataset, showCatalogDetailsSection: false },
+      callbacks,
+    );
+  }
+}
