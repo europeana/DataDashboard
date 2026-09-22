a57ca07063e258982e03f565abd410ac28e652e6
Shwetambara Nazare <nshweta90@gmail.com>
2026-09-22 12:25:40 +0200

    Update padding for logo displayt as per requirements

--- src/app/navbar-logo/navbar-logo.component.ts
+++ src/app/navbar-logo/navbar-logo.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   selector: 'app-navbar-logo',
   standalone: true,
   template: `
-    <a href="/" class="ml-2 flex items-center shrink-0" aria-label="Home">
+    <a href="/" class="flex items-center shrink-0 pl-[10px]" aria-label="Home">
       <img src="dataspace-logo.svg" alt="Home" class="h-16 w-auto" />
     </a>
   `,
